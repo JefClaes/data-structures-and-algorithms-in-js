c4771fc7d28b4c29abd212e6855921840789ca57
Jef Claes <jef.claes@gmail.com>
2011-07-23 14:31:30 +0200

    First commit.

---
+++ JavaScriptAlgorithmsDataStructs/Scripts/Algorithms.js
@@ -0,0 +1,59 @@
+﻿var algorithms = {         
+    sortArray : function(){
+        var elements = [];                                                                   
+        
+        var swap = function(one, two) {
+            var tmp = elements[one];
+            elements[one] = elements[two];
+            elements[two] = tmp;                    
+        };    
+        
+        this.onSort = function() {};              
+        this.push = function(val){
+            elements.push(val);                    
+        };                           
+        this.bubbleSort = function() {   
+            this.onSort(elements);
+			//Loop over all the elements
+            for (var out = elements.length - 1; out > 0; out--){
+				
+                for (var inn = 0; inn < out; inn++) {
+                    //Are they out of order?
+                    if (elements[inn] > elements[inn+1]){
+                        swap(inn, inn+1);                                
+                    } 
+                }
+                this.onSort(elements);
+            }                    
+        };
+        this.selectionSort = function(){
+            this.onSort(elements);
+            for (var out = 0; out < elements.length - 1; out++) {
+                var min = out;
+                for (var inn = out; inn < elements.length; inn++) {
+                    //If minium greater => new minimum
+                    if (elements[inn] < elements[min]){
+                        min = inn;
+                    }                            
+                }
+                swap(out, min);
+                this.onSort(elements);
+            }
+        };
+        this.insertionSort = function(){
+            this.onSort(elements);
+            for (var out = 1; out < elements.length; out++){                        
+                var temp = elements[out];
+                var inn = out;
+                
+                //Until one is smaller
+                while (inn > 0 && elements[inn-1] >= temp){
+                    elements[inn] = elements[inn-1];
+                    inn--;
+                }
+                elements[inn] = temp;
+                this.onSort(elements);
+            }
+        }
+    } 
+}
---
+++ JavaScriptAlgorithmsDataStructs/Scripts/DataStructures.js
@@ -0,0 +1,175 @@
+﻿var dataStructures = {                
+    node : function(data) {
+        this.data = data;                    
+        this.previous = null;
+        this.next = null;
+    },
+    linkedList : function() {
+        var first = null;
+        var last = null;
+      
+        this.getFirst = function() {
+            return first;
+        };
+        this.getLast = function() {
+            return last;  
+        };                  
+        this.insertFirst = function(value) {
+            var newNode = new dataStructures.node(value);                       
+            
+            if (first === null) {                                                        
+                last = newNode;            
+            } else {                                      
+                first.previous = newNode;
+                newNode.next = first;
+            }                                               
+            first = newNode;                       
+        };
+        this.insertLast = function(value) {
+            var newNode = new dataStructures.node(value);
+            
+            if (last === null) {
+                first = newNode;                            
+            } else {
+                last.next = newNode;
+                newNode.previous = last;
+            }
+            last = newNode;                                             
+        };
+        this.deleteFirst = function() {                                                                      
+            if (first === null){
+                return false;
+            } else {
+                if (first.next === null) {
+                    last = null;
+                } else {
+                    first.next.previous = null;
+                }
+            
+                first = first.next;
+            
+                return true;
+            }                      
+        };
+        this.deleteLast = function() {
+            if (last === null) {
+                return false;
+            }  else {
+                if (first.next === null) {
+                    first = null;
+                } else {
+                    last.previous.next = null;
+                }
+                
+                last = last.previous;
+                
+                return true;
+            }                      
+        };
+        this.insertAfter = function(key, value) {                            
+            if (first === null) {
+                return false;
+            }
+        
+            var current = first;                       
+                                   
+            while (current.data !== key) {
+                current = current.next;
+                if (current === null) {
+                    return false;
+                }
+            }
+            
+            var newNode = new dataStructures.node(value);
+            
+            if (current === last) {
+                newNode.next = null;
+                last = newNode;
+            } else {
+                newNode.next = current.next;
+                current.next.previous = newNode;
+            }
+            
+            newNode.previous = current;
+            current.next = newNode;
+            
+            return true;
+        };   
+        this.deleteKey = function(key) {                            
+            if (first === null) {
+                return false;
+            }
+        
+            var current = first;                       
+                                   
+            while (current.data !== key) {
+                current = current.next;
+                if (current === null) {
+                    return false;
+                }
+            }                       
+            
+            if (current === first) {
+                first = current.next;
+            } else {
+                current.previous.next = current.next;
+            }
+            
+            if (current === last) {
+                last = current.previous;
+            } else {
+                current.next.previous = current.previous;
+            }
+            
+            return true;
+        };                       
+        this.traverseForwards = function(callback) {
+            var current = first;                       
+                                    
+            while (current) {                            
+                callback(current);                                                     
+                current = current.next;
+            }
+        };
+        this.traverseBackwards = function(callback) {
+            var current = last;
+            
+            while (current) {
+                callback(current);
+                current = current.previous;
+            }
+        }
+    },
+    stack : function() {                  
+        var elements;
+        
+        this.push = function(element) {
+            if (typeof(elements) === 'undefined') {
+                elements = [];   
+            }                            
+            elements.push(element);
+        }
+        this.pop = function() {
+            return elements.pop();
+        }
+        this.stackTop = function(element) {
+            return elements[elements.length - 1];
+        }
+    },
+    queue : function() {
+        var elements;
+        
+        this.enqueue = function(element) {
+            if (typeof(elements) === 'undefined') {
+                elements = [];   
+            }
+            elements.push(element);                       
+        }
+        this.dequeue = function() {
+            return elements.shift();                                                
+        }
+        this.peek = function(){
+            return elements[0];                  
+        }
+    }
+};                              
