baafa945fb765117777cdd564f1c938f85bfe85a
Jef Claes <jef.claes@gmail.com>
2011-07-24 21:23:21 +0200

    Implemented MergeSort.

--- JavaScriptAlgorithmsDataStructs/Scripts/Algorithms.js
+++ JavaScriptAlgorithmsDataStructs/Scripts/Algorithms.js
@@ -6,17 +6,58 @@
             var tmp = elements[one];
             elements[one] = elements[two];
             elements[two] = tmp;                    
-        };    
+        };           
+             
+        internalMergeSort = function(elements, onSort){            
+            if (elements.length < 2){                               
+                return elements;  
+            }           
+                     
+            // Calculate the middle of the elements
+            var middle = Math.floor(elements.length / 2);           
+                       
+            // Divide 
+            var leftRange = elements.slice(0, middle);
+            var rightRange = elements.slice(middle, elements.length);                                                      
+           
+            // Conquer                                                                                                        
+            var mergingResult = merge(internalMergeSort(leftRange, onSort), 
+                                      internalMergeSort(rightRange, onSort));           
+                        
+            onSort(mergingResult);           
+                       
+            return mergingResult;
+        };
         
+        function merge(left, right){                      
+            var res = [];           
+            
+            while (left.length > 0 && right.length > 0) {                
+                if (left[0] <= right[0]) {
+                    res.push(left.shift());
+                } else {
+                    res.push(right.shift());
+                }                                              
+            }           
+            
+            while (left.length > 0) {                
+                res.push(left.shift());
+            }            
+            while (right.length > 0) {            
+                res.push(right.shift());
+            }
+            
+            return res;
+        };
+                    
         this.onSort = function() {};              
         this.push = function(val){
             elements.push(val);                    
-        };                           
+        };       
         this.bubbleSort = function() {   
             this.onSort(elements);
 			//Loop over all the elements
-            for (var out = elements.length - 1; out > 0; out--){
-				
+            for (var out = elements.length - 1; out > 0; out--){				
                 for (var inn = 0; inn < out; inn++) {
                     //Are they out of order?
                     if (elements[inn] > elements[inn+1]){
@@ -24,7 +65,9 @@
                     } 
                 }
                 this.onSort(elements);
-            }                    
+            }  
+            
+            return elements;
         };
         this.selectionSort = function(){
             this.onSort(elements);
@@ -39,6 +82,8 @@
                 swap(out, min);
                 this.onSort(elements);
             }
+            
+            return elements;
         };
         this.insertionSort = function(){
             this.onSort(elements);
@@ -54,6 +99,13 @@
                 elements[inn] = temp;
                 this.onSort(elements);
             }
-        }
+            
+            return elements;
+        };
+        this.mergeSort = function() {                                  
+            elements = internalMergeSort(elements, this.onSort);                      
+            
+            return elements;
+        };        
     } 
 }
